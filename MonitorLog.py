5caee7b2b0dea59c0dafe357a313aa0f55be2ada
suker <suker413@gmail.com>
2014-08-13 18:10:01 +0800

    modify

--- MonitorLog.py
+++ MonitorLog.py
@@ -14,7 +14,7 @@ python MonitorLog.py -f C:\monitor.log
   
 Created by Liangsh on 2014-08-13.
 """
-  
+
 import sys
 import os
 import getopt
@@ -22,10 +22,46 @@ import subprocess
 import time
 import codecs
 #import winsound
-  
+import smtplib
+from email.mime.text import MIMEText
+from email.Header import Header
+
+####
+#mailto_list=['hongyh@baoxian.com','liangsh@baoxian.com']
+mailto_list=['liangsh@baoxian.com',]
+mail_host="smtp.qq.com"
+mail_port='465'
+mail_user="2081276696"
+mail_pass="cninsure2013"
+mail_postfix="qq.com"
+mail_subject='一区TC监控-'+str(datetime.date.today().strftime('%Y%m%d'))
+
 ABSPATH = os.path.dirname(os.path.abspath(__file__))
 MONITERCONF = 'moniter_keyword.txt' #utf8 file
-  
+
+def send_mail(to_list,sub,content):
+    if isinstance(content,unicode):
+        content = str(content)
+    me= ("%s<"+mail_user+"@"+mail_postfix+">") % (Header('_mailFrom管理员','utf-8'),)
+    msg = MIMEText(content,_subtype='plain',_charset='utf-8')
+    if not isinstance(sub,unicode):
+        sub = unicode(sub)
+    msg['Subject'] = sub
+    msg['From'] = me
+    msg['To'] = ";".join(to_list)
+    msg["Accept-Language"]="zh-CN"
+    msg["Accept-Charset"]="ISO-8859-1,utf-8"
+    try:
+        server = smtplib.SMTP_SSL(mail_host,mail_port)
+        server.login(mail_user,mail_pass)
+        server.sendmail(me,to_list,msg.as_string())
+        server.close()
+        return True
+    except Exception, e:
+        print str(e)
+        return False
+
+
 def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:], 'hf:')
@@ -74,9 +110,9 @@ def main():
         for keyword in keywordMap:
             if line.find(keyword) > -1:
                 #winsound.PlaySound(keywordMap[keyword], winsound.SND_NODEFAULT)
-				print 'find some thing'
+				send_mail(mailto_list,mail_subject,"一区tc故障")
         #time.sleep(0.01)
     return 0
   
 if __name__ == '__main__':
-    sys.exit(main())
+    sys.exit(main()) 
